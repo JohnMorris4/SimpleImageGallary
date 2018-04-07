c700d341ad8f7a5e5b29dce2afee5a4dc4bba69c
JohnMorris4 <johnmorris@morrisje.com>
2018-04-06 21:03:35 -0400

    Stopping for the day

--- SimpleImageGallary/Controllers/ImageGalleryController.cs
+++ SimpleImageGallary/Controllers/ImageGalleryController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Data.Models;
 using SimpleImageGallery.Models;
+using System;
+using System.Collections.Generic;
 
 namespace SimpleImageGallery.Controllers
 {
@@ -11,9 +10,61 @@ namespace SimpleImageGallery.Controllers
     {
         public IActionResult Index()
         {
+            var HikingImageTags = new List<ImageTag>();
+            var CityImageTags = new List<ImageTag>();
+
+            var tag1 = new ImageTag()
+            {
+                Id = 0,
+                Description = "Adventure"
+            };
+            var tag2 = new ImageTag()
+            {
+                Id = 1,
+                Description = "Downtown"
+            };
+            var tag3 = new ImageTag()
+            {
+                Id = 2,
+                Description = "Mountain Adventure"
+            };
+            HikingImageTags.Add(tag1);
+            CityImageTags.AddRange(new List<ImageTag> { tag2, tag3 });
+            var imageList = new List<GalleryImage>()
+            {
+                new GalleryImage()
+                {
+                    Title= "Hiking Trip",
+                    Url = "https://images.pexels.com/photos/547116/pexels-photo-547116.jpeg",
+                    Created = DateTime.Now,
+                    Tags = HikingImageTags
+                },
+                new GalleryImage()
+                {
+                    Title= "City Trip",
+                    Url = "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg",
+                    Created = DateTime.Now,
+                    Tags = CityImageTags
+                },
+                new GalleryImage()
+                {
+                    Title= "Mountain Trip",
+                    Url = "https://images.pexels.com/photos/554609/pexels-photo-554609.jpeg",
+                    Created = DateTime.Now,
+                    Tags = HikingImageTags
+                },
+                new GalleryImage()
+                {
+                    Title= "New York",
+                    Url = "https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg",
+                    Created = DateTime.Now,
+                    Tags = CityImageTags
+                }
+            };
             var model = new GalleryIndexModel()
             {
-
+                Images = imageList,
+                SearchQuery = ""
             };
             return View(model);
         }
